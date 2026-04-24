022ed5796167bc1dec8f7448e57f3fc43465b44f
Ilyass Bougati <i.bougati12@gmail.com>
2026-04-24 19:21:07 +0100

    Added Vercel analytics

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
-    <div><h1>Welcome to Fashion ERP</h1></div>
+    <>
+      <div><h1>Welcome to Fashion ERP</h1></div>
+      <Analytics />
+    </>
   );
 }
