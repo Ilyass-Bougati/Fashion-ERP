34effb5d62e022bbc0c174fcbcc85e0b95671be6
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2026-04-26 16:34:48 +0100

    Dev/ilyass (#47)
    * Added Vercel analytics
    
    * Changes
    
    * Added MinIO service
    
    * Adding MinIO to the CI
    
    * Formatting the code

---
+++ server/src/main/java/com/sefault/server/image/service/ImageServiceImpl.java
@@ -0,0 +1,8 @@
+package com.sefault.server.image.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl {}
---
+++ server/src/main/java/com/sefault/server/minio/MinioProperties.java
@@ -0,0 +1,8 @@
+package com.sefault.server.minio;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties(prefix = "minio")
+public record MinioProperties(
+        @DefaultValue("http://localhost:9000") String endpoint, String accessKey, String secretKey) {}
