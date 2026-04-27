7e4e94b8b67ecacfd72953db5ba008280e190da3
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-04-27 16:42:40 +0100

    custom query for userdetails to fetch the user and all his authorities in one single db hit

--- server/src/main/java/com/sefault/server/user/repository/UserRepository.java
+++ server/src/main/java/com/sefault/server/user/repository/UserRepository.java
@@ -4,11 +4,22 @@ import com.sefault.server.user.dto.projection.UserProjection;
 import com.sefault.server.user.entity.User;
 import java.util.Optional;
 import java.util.UUID;
+
+import io.lettuce.core.dynamic.annotation.Param;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<@NonNull User, @NonNull UUID> {
     Optional<UserProjection> getUserProjectionById(UUID id);
+
+    @Query("""
+            SELECT u FROM User u
+            LEFT JOIN FETCH u.userAuthorities ua
+            LEFT JOIN FETCH ua.authority
+            WHERE u.email = :email
+    """)
+    Optional<User> findUserByEmailWithAuthorities(@Param("email") String email);
 }
