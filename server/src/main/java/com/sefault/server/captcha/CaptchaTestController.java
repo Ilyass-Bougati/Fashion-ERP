79cd3112d9f00b18658ef41880ae7278b6e4196f
Hajar ELHILALI <hajarelhilali57@gmail.com>
2026-04-27 22:02:53 +0200

    add @Profile(dev) to CaptchaTestController

--- server/src/main/java/com/sefault/server/captcha/CaptchaTestController.java
+++ server/src/main/java/com/sefault/server/captcha/CaptchaTestController.java
@@ -1,11 +1,13 @@
 package com.sefault.server.captcha;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("dev")
 @RestController
 @RequestMapping("/api/test")
 public class CaptchaTestController {
