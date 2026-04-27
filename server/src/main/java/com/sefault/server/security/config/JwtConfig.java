3b7647c1a5d7c4cee0390ca71e70231ad1a52743
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-04-27 19:30:08 +0100

    feat(security): configure Nimbus JwtEncoder and JwtDecoder beans with RSA keys

---
+++ server/src/main/java/com/sefault/server/security/config/JwtConfig.java
@@ -0,0 +1,38 @@
+package com.sefault.server.security.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.sefault.server.properties.JwtProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties.class)
+@RequiredArgsConstructor
+public class JwtConfig {
+    private final JwtProperties jwt;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(jwt.accessTokenPublicKey()).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(jwt.accessTokenPublicKey())
+                .privateKey(jwt.accessTokenPrivateKey())
+                .build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+}
