3a93ab2da379e7b605e6e94ff786154d1a916cb7
AmineSidki <amineyoutubw@gmail.com>
2026-04-27 15:10:49 +0100

    added LLM service

---
+++ server/src/main/java/com/sefault/server/ai/config/LlmConfiguration.java
@@ -0,0 +1,17 @@
+package com.sefault.server.ai.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class LlmConfiguration {
+    private final ChatClient.Builder chatClientBuilder;
+
+    @Bean
+    public ChatClient chatClient(){
+        return chatClientBuilder.defaultSystem("You are an expert at writing reports").build();
+    }
+}
---
+++ server/src/main/java/com/sefault/server/ai/controller/LlmTestController.java
@@ -0,0 +1,22 @@
+package com.sefault.server.ai.controller;
+
+import com.sefault.server.ai.service.LlmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile("dev")
+@RestController
+@RequestMapping("/dev/llm")
+@RequiredArgsConstructor
+public class LlmTestController {
+    private final LlmService llmService;
+
+    @PostMapping
+    public String prompt(@RequestBody String prompt){
+        return llmService.prompt(prompt);
+    }
+}
