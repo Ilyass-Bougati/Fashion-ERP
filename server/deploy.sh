4730ef61afd4fedbe67eb4a8af9ffa6195083f7b
Ilyass Bougati <i.bougati12@gmail.com>
2026-04-29 19:04:23 +0100

    Compiling the application locally

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+if [ ! -d "src/main/resources/certs" ]; then
+    echo "Certs not found, generating..."
+    chmod +x keygen.sh
+    ./keygen.sh
+fi
+
+echo "Packaging the application"
+mvn package -DskipTests=true
+
 echo "Running the CD"
 docker compose down
 docker compose up --build -d
