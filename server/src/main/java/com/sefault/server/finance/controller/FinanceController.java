d9fed18fbad027839b0579fb0a0333342538a590
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-04-30 18:40:31 +0100

    impelemt method secutity

--- server/src/main/java/com/sefault/server/finance/controller/FinanceController.java
+++ server/src/main/java/com/sefault/server/finance/controller/FinanceController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -24,21 +25,25 @@ public class FinanceController {
     private final FinanceService financeService;
 
     @PostMapping("/transactions")
+    @PreAuthorize("hasAuthority(@authorities.createTransactionAuthority)")
     public ResponseEntity<TransactionRecord> createTransactionRecord(@RequestBody @Valid TransactionRecord transactionRecord){
         return ResponseEntity.ok(financeService.createTransaction(transactionRecord));
     }
 
     @GetMapping("/transactions/{id}")
+    @PreAuthorize("hasAuthority(@authorities.readTransactionAuthority)")
     public ResponseEntity<TransactionRecord> getTransaction(@PathVariable UUID id){
         return ResponseEntity.ok(financeService.getTransaction(id));
     }
 
     @PostMapping("/transactions/{id}/reverse")
+    @PreAuthorize("hasAuthority(@authorities.reverseTransactionAuthority)")
     public ResponseEntity<TransactionRecord> reverseTransaction(@PathVariable UUID id){
         return ResponseEntity.ok(financeService.reverseTransaction(id));
     }
 
     @GetMapping("/transactions")
+    @PreAuthorize("hasAuthority(@authorities.readTransactionAuthority)")
     public ResponseEntity<Page<TransactionRecord>> getAllTransactions(
             @RequestParam(required = false) TransactionType type,
             @PageableDefault(size = 20) Pageable pageable){
@@ -49,6 +54,7 @@ public class FinanceController {
     }
 
     @PostMapping("/payroll/process/{employeeId}")
+    @PreAuthorize("hasAuthority(@authorities.processPayrollAuthority)")
     public ResponseEntity<PayrollRecord> payrollProcess(
             @PathVariable UUID employeeId,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startDate,
@@ -57,16 +63,19 @@ public class FinanceController {
     }
 
     @GetMapping("/payroll/{id}")
+    @PreAuthorize("hasAnyAuthority(@authorities.readPayrollAuthority)")
     public ResponseEntity<PayrollRecord> getPayroll(@PathVariable UUID id) {
         return ResponseEntity.ok(financeService.getPayroll(id));
     }
 
     @GetMapping("/payroll")
+    @PreAuthorize("hasAnyAuthority(@authorities.readPayrollAuthority)")
     public ResponseEntity<Page<PayrollRecord>> getAllPayrolls(@PageableDefault(size = 20) Pageable pageable) {
         return ResponseEntity.ok(financeService.getAllPayrolls(pageable));
     }
 
     @GetMapping("/payroll/employee/{employeeId}")
+    @PreAuthorize("hasAnyAuthority(@authorities.readPayrollAuthority)")
     public ResponseEntity<Page<PayrollRecord>> getPayrollHistoryForEmployee(
             @PathVariable UUID employeeId,
             @PageableDefault(size = 20) Pageable pageable) {
@@ -74,27 +83,32 @@ public class FinanceController {
     }
 
     @PostMapping("/fixed-charges")
+    @PreAuthorize("hasAuthority(@authorities.createFixedChargeAuthority)")
     public ResponseEntity<FixChargeRecord> createFixCharge(@RequestBody FixChargeRecord record) {
         return ResponseEntity.ok(financeService.createFixCharge(record));
     }
 
     @GetMapping("/fixed-charges/{id}")
+    @PreAuthorize("hasAuthority(@authorities.readFixedChargeAuthority)")
     public ResponseEntity<FixChargeRecord> getFixCharge(@PathVariable UUID id) {
         return ResponseEntity.ok(financeService.getFixCharge(id));
     }
 
     @PutMapping("/fixed-charges")
+    @PreAuthorize("hasAuthority(@authorities.updateFixedChargeAuthority)")
     public ResponseEntity<FixChargeRecord> updateFixCharge(@RequestBody FixChargeRecord record) {
         return ResponseEntity.ok(financeService.updateFixCharge(record));
     }
 
     @PatchMapping("/fixed-charges/{id}/toggle")
+    @PreAuthorize("hasAuthority(@authorities.toggleFixedChargeAuthority)")
     public ResponseEntity<Void> toggleFixChargeStatus(@PathVariable UUID id) {
         financeService.toggleFixChargeStatus(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/fixed-charges")
+    @PreAuthorize("hasAuthority(@authorities.readFixedChargeAuthority)")
     public ResponseEntity<Page<FixChargeRecord>> getAllFixCharges(
             @RequestParam(required = false, defaultValue = "false") boolean activeOnly,
             @PageableDefault(size = 20) Pageable pageable) {
