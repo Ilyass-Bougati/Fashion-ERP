f097214bbdb09796c661d36f25b2bbae3acc8401
Hajar ELHILALI <hajarelhilali57@gmail.com>
2026-04-30 02:47:43 +0200

    HR module authorities

--- server/src/main/java/com/sefault/server/security/properties/ApplicationAuthorities.java
+++ server/src/main/java/com/sefault/server/security/properties/ApplicationAuthorities.java
@@ -14,4 +14,19 @@ public class ApplicationAuthorities {
     private final String activateUsersAuthority = "ACTIVATE_USERS";
     private final String getUsersAuthority = "GET_USERS";
     private final String listUsersAuthority = "LIST_USERS";
+
+    // HR module authorities
+    private final String createEmployeeAuthority = "CREATE_EMPLOYEE";
+    private final String updateEmployeeAuthority = "UPDATE_EMPLOYEE";
+    private final String deleteEmployeeAuthority = "DELETE_EMPLOYEE";
+    private final String getEmployeeAuthority = "GET_EMPLOYEE";
+    private final String listEmployeesAuthority = "LIST_EMPLOYEES";
+    private final String terminateEmployeeAuthority = "TERMINATE_EMPLOYEE";
+
+    private final String createIsleAuthority = "CREATE_ISLE";
+    private final String updateIsleAuthority = "UPDATE_ISLE";
+    private final String deleteIsleAuthority = "DELETE_ISLE";
+    private final String getIsleAuthority = "GET_ISLE";
+    private final String listIslesAuthority = "LIST_ISLES";
+    private final String assignIsleAuthority = "ASSIGN_ISLE";
 }
