72408bec81896e7062e2bde1dcac303c56e98bf9
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-04-30 18:33:01 +0100

    add the Finance module authorities

--- server/src/main/java/com/sefault/server/security/properties/ApplicationAuthorities.java
+++ server/src/main/java/com/sefault/server/security/properties/ApplicationAuthorities.java
@@ -14,4 +14,15 @@ public class ApplicationAuthorities {
     private final String activateUsersAuthority = "ACTIVATE_USERS";
     private final String getUsersAuthority = "GET_USERS";
     private final String listUsersAuthority = "LIST_USERS";
+
+    // Finance module authorities
+    private final String createTransactionAuthority = "CREATE_TRANSACTION";
+    private final String readTransactionAuthority = "READ_TRANSACTION";
+    private final String reverseTransactionAuthority = "REVERSE_TRANSACTION";
+    private final String createFixedChargeAuthority = "CREATE_FIXED_CHARGE";
+    private final String readFixedChargeAuthority = "READ_FIXED_CHARGE";
+    private final String updateFixedChargeAuthority = "UPDATE_FIXED_CHARGE";
+    private final String toggleFixedChargeAuthority = "TOGGLE_FIXED_CHARGE";
+    private final String processPayrollAuthority = "PROCESS_PAYROLL";
+    private final String readPayrollAuthority = "READ_PAYROLL";
 }
