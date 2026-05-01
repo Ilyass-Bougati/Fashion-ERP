44943b6959e73db5fcea225efa79aa41ab04da38
Ilyass Bougati <i.bougati12@gmail.com>
2026-05-01 12:21:40 +0100

    Allowing Swagger-UI on the staging environment

--- server/src/main/java/com/sefault/server/captcha/CaptchaTestController.java
+++ server/src/main/java/com/sefault/server/captcha/CaptchaTestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("dev")
+@Profile({"dev", "staging"})
 @RestController
 @RequestMapping("/api/test")
 @RequiredArgsConstructor
