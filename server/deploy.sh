f64455ccaf0f317d1699a53f272d5868a325aa11
Ilyass Bougati <i.bougati12@gmail.com>
2026-05-01 12:57:57 +0100

    Fix

--- server/deploy.sh
+++ server/deploy.sh
@@ -9,17 +9,6 @@ fi
 echo "Packaging the application"
 mvn package -DskipTests=true
 
-#!/usr/bin/bash
-
-if [ ! -d "src/main/resources/certs" ]; then
-    echo "Certs not found, generating..."
-    chmod +x keygen.sh
-    ./keygen.sh
-fi
-
-echo "Packaging the application"
-mvn package -DskipTests=true
-
 echo "Running the CD"
 docker compose up --build -d server
 docker image prune -f
