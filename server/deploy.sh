d4859991a5d8f23930ef97d2676e59720ce4c15e
Ilyass Bougati <i.bougati12@gmail.com>
2026-05-01 12:56:10 +0100

    A faster CD

--- server/deploy.sh
+++ server/deploy.sh
@@ -9,7 +9,17 @@ fi
 echo "Packaging the application"
 mvn package -DskipTests=true
 
+#!/usr/bin/bash
+
+if [ ! -d "src/main/resources/certs" ]; then
+    echo "Certs not found, generating..."
+    chmod +x keygen.sh
+    ./keygen.sh
+fi
+
+echo "Packaging the application"
+mvn package -DskipTests=true
+
 echo "Running the CD"
-docker compose down
-docker compose up --build -d
+docker compose up --build -d server
 docker image prune -f
