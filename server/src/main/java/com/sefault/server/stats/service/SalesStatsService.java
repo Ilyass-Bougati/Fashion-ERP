c9cff8b046ceffd8a22e96bd62d04a23e39935b0
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-05-06 19:16:30 +0100

    fixes

--- server/src/main/java/com/sefault/server/stats/service/SalesStatsService.java
+++ server/src/main/java/com/sefault/server/stats/service/SalesStatsService.java
@@ -1,4 +1,10 @@
 package com.sefault.server.stats.service;
 
+import com.sefault.server.stats.enums.PeriodType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface SalesStatsService {
+    void saveSalesStats(LocalDateTime start, LocalDateTime end, LocalDate anchorDate, PeriodType periodType);
 }
