640a7f9a0aee40f8af23eec6d4a81ee0bbe7123d
abdellah-darni <abdellah.darni.2018@gmail.com>
2026-05-12 21:15:45 +0100

    the prediction query service

---
+++ server/src/main/java/com/sefault/server/prediction/service/PredictionQueryService.java
@@ -0,0 +1,18 @@
+package com.sefault.server.prediction.service;
+
+import com.sefault.server.prediction.dto.record.*;
+import com.sefault.server.stats.enums.PeriodType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+public interface PredictionQueryService {
+    Page<SalesPredictionRecord> getSalesPredictions(LocalDate targetDateFrom, PeriodType periodType, Pageable pageable);
+
+    Page<StockPredictionRecord> getStockPredictions(LocalDate targetDateFrom, PeriodType periodType, String sku, Pageable pageable);
+
+    Page<FinancialPredictionRecord> getFinancialPredictions(LocalDate targetDateFrom, PeriodType periodType, Pageable pageable);
+
+    Page<EmployeePerformancePredictionRecord> getEmployeePredictions(LocalDate targetDateFrom, PeriodType periodType, String cin, Pageable pageable);
+}
