4f042ab8437e6548bf0241b1308448338d4ea61c
Ilyass-Bougati <i.bougati12@gmail.com>
2026-05-13 13:40:40 +0100

    Fixes

--- server/src/main/java/com/sefault/server/user/repository/UserAuthorityRepository.java
+++ server/src/main/java/com/sefault/server/user/repository/UserAuthorityRepository.java
@@ -2,15 +2,21 @@ package com.sefault.server.user.repository;
 
 import com.sefault.server.user.dto.projection.UserAuthorityProjection;
 import com.sefault.server.user.entity.UserAuthority;
+import com.sefault.server.user.entity.id.UserAuthorityId;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserAuthorityRepository extends JpaRepository<@NonNull UserAuthority, @NonNull UUID> {
-    Optional<UserAuthorityProjection> getUserAuthorityProjectionById(UUID id);
+public interface UserAuthorityRepository extends JpaRepository<@NonNull UserAuthority, @NonNull UserAuthorityId> {
+    Optional<UserAuthorityProjection> getUserAuthorityProjectionById(UserAuthorityId id);
 
-    void deleteByUser_IdAndAuthority_Id(UUID userId, UUID authorityId);
+    @Modifying
+    @Query("DELETE FROM UserAuthority ua WHERE ua.user.id = :userId AND ua.authority.id = :authorityId")
+    void deleteByUser_IdAndAuthority_Id(@Param("userId") UUID userId, @Param("authorityId") UUID authorityId);
 }
