c1337dad7f422288841cb3eadd49265463251c3e
Ilyass-Bougati <i.bougati12@gmail.com>
2026-05-19 12:50:22 +0100

    Changed a lot

--- server/src/main/java/com/sefault/server/sales/service/Impl/SaleServiceImpl.java
+++ server/src/main/java/com/sefault/server/sales/service/Impl/SaleServiceImpl.java
@@ -13,13 +13,14 @@ import com.sefault.server.sales.mapper.SaleMapper;
 import com.sefault.server.sales.repository.SaleRepository;
 import com.sefault.server.sales.service.SaleService;
 import com.sefault.server.storage.repository.ProductVariationRepository;
-import java.time.LocalDateTime;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -38,6 +39,9 @@ public class SaleServiceImpl implements SaleService {
         if (record.employeeId() != null) {
             sale.setEmployee(employeeRepository.getReferenceById(record.employeeId()));
         }
+        // when creating a sale, the status should always be PENDING
+        sale.setStatus(SaleStatus.PENDING);
+        
         return saleMapper.entityToRecord(saleRepository.save(sale));
     }
 
@@ -54,6 +58,12 @@ public class SaleServiceImpl implements SaleService {
     @Transactional(readOnly = true)
     public Page<SaleRecord> getAll(Pageable pageable) {
         return saleRepository.findAllBy(pageable).map(saleMapper::projectionToRecord);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<SaleRecord> getByEmployee(UUID employeeId, Pageable pageable) {
+        return saleRepository.findAllByEmployeeId(employeeId, pageable).map(saleMapper::projectionToRecord);
     }
 
     private Sale findEntityOrThrow(UUID id) {
--- server/src/main/java/com/sefault/server/user/repository/UserAuthorityRepository.java
+++ server/src/main/java/com/sefault/server/user/repository/UserAuthorityRepository.java
@@ -6,11 +6,16 @@ import java.util.Optional;
 import java.util.UUID;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserAuthorityRepository extends JpaRepository<@NonNull UserAuthority, @NonNull UUID> {
     Optional<UserAuthorityProjection> getUserAuthorityProjectionById(UUID id);
 
-    void deleteByUser_IdAndAuthority_Id(UUID userId, UUID authorityId);
+    @Modifying
+    @Query("DELETE FROM UserAuthority ua WHERE ua.user.id = :userId AND ua.authority.id = :authorityId")
+    void deleteByUser_IdAndAuthority_Id(@Param("userId") UUID userId, @Param("authorityId") UUID authorityId);
 }
